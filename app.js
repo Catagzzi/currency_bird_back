d49fe68f3359d4f6dc5f50b5f19f5c548400f28e
Catagzzi <catagzzi@gmail.com>
2022-04-28 11:47:00 -0400

    Se crean endpoints de CRUD de ejemplo

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbName = process.env.DB_NAME;
-const dbPort = process.env.DB_PORT;
-
-console.log(dbHost);
 
 //Mysql
 const connection =  mysql.createConnection({
@@ -21,8 +18,62 @@ const connection =  mysql.createConnection({
     user: dbUser, //'root',
     password: dbPass, //'Cata1048576_',
     database: dbName, //'currency_bird',
-    port: dbPort
+    port: 3306
 });
+
+// Route
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+});
+
+// ----------------------------------------------------------------------------
+// CRUD EJEMPLO
+app.get('/resumen', (req, res) => {
+    const sqlGetTablaResumen = 'SELECT * FROM usuario';
+    connection.query(sqlGetTablaResumen, (err, results) => {
+        if (err) throw err;
+        if (results.length > 0){
+            res.json(results);
+        } else {
+            res.send('Sin resultados');
+        }
+    });
+});
+
+app.post('/agregar', (req, res) => {
+    const sqlInsetUsuario = 'INSERT INTO usuario SET ?';
+    const usuarioObject = {
+        nombre: req.body.nombre,
+        email: req.body.email,
+        direccion: req.body.direccion,
+        sexo: req.body.sexo
+    };
+    connection.query(sqlInsetUsuario, usuarioObject, error => {
+        if (error) throw error;
+        res.send('Usuario creado correctamente');
+    })
+});
+
+app.put('/update/:id', (req, res) => {
+    const {id } = req.params;
+    const {email, direccion} = req.body;
+    const sqlUpdateUsuario = `UPDATE usuario SET Direccion = '${direccion}', Email = '${email}'  WHERE id = ${id}`;
+    console.log(direccion);
+    connection.query(sqlUpdateUsuario, error => {
+        if (error) throw error;
+        res.send('Usuario actualizado correctamente');
+    });
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    const sqlDeleteUsuario = `DELETE FROM usuario WHERE id = ${id}`;
+    connection.query(sqlDeleteUsuario, error => {
+        if (error) throw error;
+        res.send('Usuario eliminado correctamente');
+    });
+});
+// ----------------------------------------------------------------------------
 
 //Check connection
 connection.connect(error => {
@@ -32,6 +83,6 @@ connection.connect(error => {
     console.log('Database server running');
 });
 
-app.listen(PORT, () => `Server running on port ${PORT}`);
+app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`));
 
 
